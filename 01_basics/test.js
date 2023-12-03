c725119b08290e3f8c6ea0d5bea38e8443233cfb
Shehzab <106922099+shehzab@users.noreply.github.com>
2023-12-03 11:04:32 +0530

    Update test.js

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,25 @@
-console.log("shehzab")
+onst accId= 122345;
+let email = "eg@gmsil.com";
+var psswrd= "123shrjhsk"
+accCity = "jaipur";
+
+let accState;
+
+
+console.log(accCity)
+
+// accId = 2 
+console.log(accId)
+
+email = "shshsh@gmalksmm";
+console.log(email)
+
+psswrd = "124563432"
+accCity = "bengakuru"
+
+console.log(psswrd , accCity,accState)
+
+/*
+prefer not to use var
+ 
+*/
