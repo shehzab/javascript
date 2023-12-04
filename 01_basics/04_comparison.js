331c6cf6c22b009e15e7297acbdb3f050ee7ba3e
Shehzab <106922099+shehzab@users.noreply.github.com>
2023-12-04 22:10:25 +0530

    Create 04_comparison.js

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+console.log(2 > 1)
+console.log(2 >= 1)
+console.log(2 < 1)
+console.log(2 <= 1)
+console.log(2 == 1)
+console.log(2 != 1)
+
+
+// (5 comparisons under this passage) this type of comparisons can make us confused so avouid these type of comparisons 
+// write clean code as possible 
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+
+// === strict equality operator
+
+console.log ("2" == 2) //true
+console.log ("2" === 2) //false 
+// in === it checks datatype also so it gives output as false
