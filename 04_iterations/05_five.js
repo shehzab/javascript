0d12a774af04fab1d9ef7229e5a722b9fb702c94
Shehzab <106922099+shehzab@users.noreply.github.com>
2023-12-20 13:33:42 +0530

    Create 05_five.js

---
+++ 04_iterations/05_five.js
@@ -0,0 +1,49 @@
+// foreach
+
+const coding = ["js","ruby","python","cpp"]
+
+coding.forEach(function (val) {
+    // console.log(val);
+})
+
+coding.forEach( (item) => {
+    // console.log(item)
+})
+
+// function printMe(item){
+//     console.log(item)
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach((item , index, arr) => {
+//     console.log(item,index,arr)
+// })
+
+
+const myCoding = [
+    {
+        languageName :"javascript",
+        languageFilename :"js"
+    },
+    {
+        languageName :"java",
+        languageFilename :"java"
+    },
+    {
+        languageName :"python",
+        languageFilename :"py"
+    },
+    {
+        languageName :"C++",
+        languageFilename :"cpp"
+    }
+]
+
+myCoding.forEach((item) => {
+
+    console.log(item.languageName)
+
+})
+
+
