0fefd5dde20b2e71046a58e66c1adf8b4b9b58fc
Shehzab <106922099+shehzab@users.noreply.github.com>
2023-12-20 11:37:16 +0530

    Create Two.js

---
+++ 04_iterations/Two.js
@@ -0,0 +1,25 @@
+// while and do-while
+let index = 0
+while (index <= 10) {
+    //  console.log("value of index", index)
+     index++
+}
+
+let myArr = ['flash', 'batman', 'superman']
+
+let arr = 0
+while (arr < myArr.length) {
+    // console.log(`value is ${myArr[arr]}`)
+    // arr = arr + 1
+    arr++
+}
+
+
+
+let myscore = 1
+
+do {
+ console.log(`score is ${myscore}`) 
+ myscore++   
+} while (myscore <= 10);
+
