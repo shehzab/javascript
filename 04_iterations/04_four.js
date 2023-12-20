a89cc3c325408a659c3cd5932842fbca2b11fa6f
Shehzab <106922099+shehzab@users.noreply.github.com>
2023-12-20 13:08:59 +0530

    Update 04_four.js

--- 04_iterations/04_four.js
+++ 04_iterations/04_four.js
@@ -1,1 +1,18 @@
 
+const myObject = {
+    js: 'javascript',
+    cpp : 'c++',
+    rb: 'ruby',
+    swift :'swift by apple'
+}
+
+for (const key in myObject) {
+    // console.log(`${key} is short for ${myObject[key]}`)
+}
+
+const programming =["js", "rb", "py","java","c"]
+
+for (const key in programming) {
+    // console.log(key);// output: 0,1,2,3,4
+    console.log(programming[key])
+}
